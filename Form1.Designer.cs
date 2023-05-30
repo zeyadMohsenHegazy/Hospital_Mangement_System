854b1f460c9f80c263f3bb0addb660091860cd67
Zeyad_Mohsen <zeyadmohsen1999@gmail.com>
2023-05-30 16:10:19 +0300

    updated

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -59,7 +59,7 @@
             this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.guna2Button1.ForeColor = System.Drawing.Color.White;
-            this.guna2Button1.Location = new System.Drawing.Point(209, 216);
+            this.guna2Button1.Location = new System.Drawing.Point(291, 173);
             this.guna2Button1.Name = "guna2Button1";
             this.guna2Button1.Size = new System.Drawing.Size(180, 45);
             this.guna2Button1.TabIndex = 3;
