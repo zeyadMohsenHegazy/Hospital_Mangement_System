cee5d284f3c49ae3ed6f0120a00574497f7bdc08
Zeyad_Mohsen <zeyadmohsen1999@gmail.com>
2023-06-01 03:20:30 +0300

    zeyad update

--- Program.cs
+++ Program.cs
@@ -16,7 +16,6 @@ namespace HospitalMangementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Main());
             Application.Run(new Form1());
         }
     }
